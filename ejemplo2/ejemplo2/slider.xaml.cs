53f8476e5548e005bb826e99147700de0a844174
mac <mac4@imac06>
2023-03-17 18:42:19 -0500

    firts commit

---
+++ ejemplo2/ejemplo2/slider.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejemplo2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class slider : ContentPage
+    {
+        public slider()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Numero:{0:F2}", e.NewValue);
+        }
+    }
+}
